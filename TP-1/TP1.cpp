53ea5eea1595ff374b2ce913e173077b1f19ae11
Catarina Springer Moreira <147004676+c-springer@users.noreply.github.com>
2025-09-24 11:14:33 +0000

    Inplementação da media, desvio padrão e preparação para implementar a tabela

--- TP-1/TP1.cpp
+++ TP-1/TP1.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 
 int main()
 {
@@ -35,9 +36,8 @@ int main()
 
     valores = new float[nelems]; // criação do array dinâmico
     
-    if (valores == NULL)
-    {// erro na alocação de memória
-
+    if (valores == NULL) // erro na alocação de memória
+    {
         printf("Erro na alocação de memória\n");
         return 1; // termina o programa
     }
@@ -53,15 +53,19 @@ int main()
     ///////////////////////////////////////////////////////////
     // C O M P L E T A R
 
-    float temp, amplitude;
-    int n = 0;
+    float temp = 0, amp_elems = 0,val_min ,val_max , amp_classes = 0, media, sum_elems, dp, sum_dp;
+    
+    int n, j;
 
     do //algoritmo para organizar os elementos por ordem crescente
     {
-        for (int j = 0; j < (nelems - 1); j++)
+        n = 0;
+
+        for (j = 0; j < (nelems - 1); j++)
         {
             if (valores[j] > valores[j + 1])
             {
+                // printf("%.2f \n", valores[j]);
                 temp = valores[j];
                 valores[j] = valores[j + 1];
                 valores[j + 1] = temp;
@@ -72,10 +76,38 @@ int main()
         
     } while (n > 0);
 
+    printf("%.2f\n", valores);
+    printf("v max: %.2f\n", valores[nelems - 1]);
+    printf("v min: %.2f\n", valores[0]);
+
+    val_min = valores[0];
+    val_max = valores[nelems - 1];
+
+    amp_elems = val_max - val_min; //amplitude dos elementos
+
+    amp_classes = amp_elems / nclasses; //amplitude por classe
+
+    printf("amplitude: %.2f\n\n", amp_elems);
 
 
 
     
+
+
+    for (int a = 0; i < nelems; a++) //loop para somatorio
+    {
+        sum_elems += valores[a];
+    }
+    media = sum_elems / nelems; //calculo da media
+    printf("\nMédia: %.2f\n", media);
+
+    for (int b = 0; b < nelems; b++) //loop para somatorio do desvio-padrao
+    {
+        sum_dp += pow(valores[b] - media, 2);
+    }
+    dp = sqrt(sum_dp / nelems); // calculo do desvio-padrao
+    printf("Desvio padrão: %.2f\n", dp);
+
     delete[] valores; // libertação da memória ocupada pelo array dinâmico
     
     return 0;
